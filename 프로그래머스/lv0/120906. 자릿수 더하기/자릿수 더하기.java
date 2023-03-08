0c6370517aef4c5bd0c01904ef9e9c5f86c3625b
lsh4711 <120231876+lsh4711@users.noreply.github.com>
2023-03-08 17:55:22 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -2,7 +2,7 @@ class Solution {
     public int solution(int n) {
         int answer = 0;
         
-        for(String i : (Integer.toString(n)).split("")) answer += Integer.parseInt(i);
+        for(char c : Integer.toString(n).toCharArray()) answer += c - '0';
         
         return answer;
     }
