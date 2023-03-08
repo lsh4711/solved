de302c5672a246e686973c115d6ff5315203f9eb
lsh4711 <120231876+lsh4711@users.noreply.github.com>
2023-03-08 17:45:09 +0900

    [level 0] Title: OX퀴즈, Time: 0.11 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.java
+++ 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.java
@@ -2,7 +2,10 @@ class Solution {
     public String[] solution(String[] quiz) {
         for(int i=0; i<quiz.length; i++) {
             String[] str = quiz[i].split(" ");
-            quiz[i] = (Integer.parseInt(str[0])+Integer.parseInt(str[2])*(str[1].equals("+") ? 1 : -1) == Integer.parseInt(str[4])) ? "O" : "X";
+            int op = 1;
+            if(str[1].equals("-")) op = -1;
+            quiz[i] = "O";
+            if(Integer.parseInt(str[0])+Integer.parseInt(str[2])*op != Integer.parseInt(str[4])) quiz[i] = "X";
         }
         
         return quiz;
