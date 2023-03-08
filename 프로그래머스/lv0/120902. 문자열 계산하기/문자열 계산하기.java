53370d08995f691a536c3ec547c8245734f56e63
lsh4711 <120231876+lsh4711@users.noreply.github.com>
2023-03-08 18:19:56 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.11 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.java
+++ 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.java
@@ -1,11 +1,11 @@
 class Solution {
     public int solution(String my_string) {
-        String[] strarr = my_string.split(" ");
-        int answer = Integer.parseInt(strarr[0]);
+        String[] arr = my_string.split(" ");
+        int answer = Integer.parseInt(arr[0]);
         
-        for(int i=2; i<strarr.length; i++) {
-            if(strarr[i-1].equals("+")) answer += Integer.parseInt(strarr[i]);
-            else if(strarr[i-1].equals("-")) answer -= Integer.parseInt(strarr[i]);
+        for(int i=2; i<arr.length; i+=2) {
+            if(arr[i-1].equals("+")) answer += Integer.parseInt(arr[i]);
+            else if(arr[i-1].equals("-")) answer -= Integer.parseInt(arr[i]);
         }
         
         return answer;
