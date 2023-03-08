496a954257bf30134e0672f5c122f771f8d28a79
lsh4711 <120231876+lsh4711@users.noreply.github.com>
2023-03-08 15:45:17 +0900

    [level 0] Title: 옹알이 (1), Time: 0.55 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.java
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -2,8 +2,10 @@ class Solution {
     public int solution(String[] babbling) {
         int answer =0;
         
-        for(String str : babbling) answer += str.replaceAll("aya|ye|woo|ma", "").equals("") ? 1 : 0;
+        for(String str : babbling) {
+            if(str.replaceAll("aya|ye|woo|ma", "").length() == 0) answer++;
+        }
         
-        return answer;
+        return answer; 
     }
 }
