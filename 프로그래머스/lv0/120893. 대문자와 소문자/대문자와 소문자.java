6e585b877b6939c7d959d692a7c4e5e57c95570e
lsh4711 <120231876+lsh4711@users.noreply.github.com>
2023-03-09 09:56:57 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.16 ms, Memory: 78.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String my_string) {
+        StringBuilder answer = new StringBuilder();
+        char[] arr = my_string.toCharArray();
+
+        for(char c : arr) {
+            if(c>90) answer.append((char)(c-32));
+            else answer.append((char)(c+32));
+        }
+        
+        return answer.toString();
+    }
+}
