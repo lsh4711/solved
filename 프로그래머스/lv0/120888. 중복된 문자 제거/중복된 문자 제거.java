09c0136cad2dcade7571fc53489f2cd181557bb0
lsh4711 <120231876+lsh4711@users.noreply.github.com>
2023-03-09 17:34:57 +0900

    [level 0] Title: 중복된 문자 제거, Time: 2.36 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.java
+++ 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.java
@@ -1,14 +1,10 @@
+import java.util.stream.Collectors;
+
 class Solution {
     public String solution(String my_string) {
-        StringBuilder answer = new StringBuilder();
-        
-        label : for(char c : my_string.toCharArray()) {
-            for(int i=0; i<answer.length(); i++) {
-                if(answer.charAt(i) == c) continue label;
-            }
-            answer.append(c);
-        }
-        
-        return answer.toString();
+        return my_string.chars()
+                .mapToObj(Character::toString)
+                .distinct()
+                .collect(Collectors.joining());
     }
 }
