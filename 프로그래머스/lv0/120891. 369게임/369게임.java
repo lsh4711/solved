a1fe5c45aa342f06bb266e224a959ad30240f419
lsh4711 <120231876+lsh4711@users.noreply.github.com>
2023-03-09 15:47:25 +0900

    [level 0] Title: 369게임, Time: 0.02 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120891. 369게임/369게임.java
+++ 프로그래머스/lv0/120891. 369게임/369게임.java
@@ -3,7 +3,8 @@ class Solution {
         int answer = 0;
         
         for(; order>0; order/=10) {
-            if(order%10 != 0 && order%10%3 == 0) answer ++;
+            int num = order % 10;
+            if(num%3 == 0 && num != 0) answer ++;
         }
         
         return answer;
