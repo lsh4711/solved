47ed48ef7c0f3917bfd7884c34165eb4f87e60a2
lsh4711 <120231876+lsh4711@users.noreply.github.com>
2023-03-09 09:31:08 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.02 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.java
+++ 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.java
@@ -1,7 +1,7 @@
 class Solution {
     public String solution(String s) {
-        int[] visit = new int[123];
         StringBuilder answer = new StringBuilder();
+        int[] visit = new int[123];
         
         for(int i=0; i<s.length(); i++) visit[s.charAt(i)]++;
         for(int i=65; i<123; i++) {
