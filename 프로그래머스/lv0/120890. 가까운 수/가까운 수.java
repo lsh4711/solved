abde341b4f16ac9208cb4ad8fafb90c4a39627db
lsh4711 <120231876+lsh4711@users.noreply.github.com>
2023-03-09 16:36:46 +0900

    [level 0] Title: 가까운 수, Time: 0.08 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120890. 가까운 수/가까운 수.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int[] array, int n) {
+        int answer = array[0];
+        
+        for(int i=1; i<array.length; i++) {
+            int num1 = Math.abs(answer-n);
+            int num2 = Math.abs(array[i]-n);
+            if(num1<num2) continue;
+            if(num1 == num2) answer = Math.min(answer, array[i]);
+            else answer = array[i];
+        }
+        
+        return answer;
+    }
+}
