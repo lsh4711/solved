e628a0d264ca5e19aa4ff1a801302817e29e159d
lsh4711 <120231876+lsh4711@users.noreply.github.com>
2023-03-09 10:00:30 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.11 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.java
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.java
@@ -1,9 +1,8 @@
 class Solution {
     public String solution(String my_string) {
         StringBuilder answer = new StringBuilder();
-        
-        for(int i=0; i<my_string.length(); i++) {
-            char c = my_string.charAt(i);
+
+        for(char c : my_string.toCharArray()) {
             if(c>90) answer.append((char)(c-32));
             else answer.append((char)(c+32));
         }
