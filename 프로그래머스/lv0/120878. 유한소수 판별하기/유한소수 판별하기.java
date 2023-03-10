4e71d5663fd03dc559a15ac5a6248faad56b864d
lsh4711 <120231876+lsh4711@users.noreply.github.com>
2023-03-10 16:23:07 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.03 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.java
+++ 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.java
@@ -1,17 +1,9 @@
 class Solution {
     public int solution(int a, int b) {
-        int n = b;
+        while(b%5 == 0) b /= 5;
+        while(b%2 == 0) b /= 2;
         
-        while(b != 0) {
-            int r = a % b;
-            a = b;
-            b = r;
-        }
-        n /= a;
-        while(n%5 == 0) n /= 5;
-        while(n%2 == 0) n /= 2;
-        
-        if(n == 1) return 1;
+        if(a%b == 0) return 1;
         return 2;
     }
 }
