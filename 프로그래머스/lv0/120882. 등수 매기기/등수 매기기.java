65b32643ddef2529177eee8e447fdab4d2daada6
lsh4711 <120231876+lsh4711@users.noreply.github.com>
2023-03-10 10:06:21 +0900

    [level 0] Title: 등수 매기기, Time: 0.47 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.java
+++ 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.java
@@ -2,15 +2,12 @@ import java.util.ArrayList;
 class Solution {
     public int[] solution(int[][] score) {
         ArrayList<Integer> scoreList = new ArrayList<>();
-        for(int[] t : score){
-            scoreList.add(t[0] + t[1]);
-        }
+        for(int[] t : score) scoreList.add(t[0] + t[1]);
         scoreList.sort((a1, a2) -> a2 - a1);
 
         int[] answer = new int[score.length];
-        for(int i=0; i<score.length; i++){
-            answer[i] = scoreList.indexOf(score[i][0] + score[i][1])+1;
-        }
+        for(int i=0; i<score.length; i++) answer[i] = scoreList.indexOf(score[i][0] + score[i][1])+1;
+        
         return answer;
     }
 }
