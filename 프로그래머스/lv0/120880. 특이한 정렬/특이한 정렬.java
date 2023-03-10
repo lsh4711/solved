819f6faa4aa3f97cfa17c2dd61622d3ecbe5e2d7
lsh4711 <120231876+lsh4711@users.noreply.github.com>
2023-03-10 10:19:41 +0900

    [level 0] Title: 특이한 정렬, Time: 0.26 ms, Memory: 72.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public Integer[] solution(int[] numlist, int n) {
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        for(int i : numlist) list.add(Math.abs(i-n));
+        Integer[] arr = list.toArray(new Integer[0]);
+        Arrays.sort(arr);
+        for(int i=0; i<numlist.length; i++) {
+            int io = list.indexOf(arr[i]);
+            int lio = list.lastIndexOf(arr[i]);
+            if(numlist[io] == numlist[lio]) arr[i] = numlist[io];
+            else {
+                arr[i] = Math.max(numlist[io], numlist[lio]);
+                arr[i+1] = Math.min(numlist[io], numlist[lio]);
+                i++;
+            }
+        }
+        
+        return arr;
+    }
+}
