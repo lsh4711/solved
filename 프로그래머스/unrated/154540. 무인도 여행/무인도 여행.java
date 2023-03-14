b3be07db7b7a89c3b2a931f372d311576e77b71d
lsh4711 <120231876+lsh4711@users.noreply.github.com>
2023-03-14 17:27:47 +0900

    [unrated] Title: 무인도 여행, Time: 1.34 ms, Memory: 83.3 MB -BaekjoonHub

--- 프로그래머스/unrated/154540. 무인도 여행/무인도 여행.java
+++ 프로그래머스/unrated/154540. 무인도 여행/무인도 여행.java
@@ -1,43 +1,38 @@
 import java.util.*;
 
 class Solution {
-    public List<Integer> solution(String[] maps) {
+    public ArrayList<Integer> solution(String[] maps) {
         ArrayList<Integer> answer = new ArrayList<>();
         boolean[][] visit = new boolean[maps.length][maps[0].length()];
         
         for(int i=0; i<maps.length; i++) {
             for(int j=0; j<maps[0].length(); j++) {
-                if(visit[i][j] || maps[i].charAt(j) == 'X') continue;
-                int food = bfs(maps, visit, i, j);
-                if(food == 0) answer.add(-1);
-                else answer.add(food);
+                if(!visit[i][j] && maps[i].charAt(j) != 'X') answer.add(bfs(maps, visit, i, j));
             }
         }
         answer.sort(null);
+        if(answer.size() == 0) answer.add(-1);
         
-        if(answer.size() == 0) return Arrays.asList(-1);
         return answer;
     }
     
     static int bfs(String[] maps, boolean[][] visit, int i, int j) {
         Queue<int[]> queue = new LinkedList<>();
-        int cnt = 0;
+        int[][] move = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+        int food = 0;
         
         queue.add(new int[] {i, j});
-        
         while(!queue.isEmpty()) {
             int[] poll = queue.poll();
             int row = poll[0];
             int col = poll[1];
             if(row<0 || col<0 || row>=visit.length || col>=visit[0].length || visit[row][col] || maps[row].charAt(col) == 'X') continue;
+            food += maps[row].charAt(col) - '0';
             visit[row][col] = true;
-            cnt += maps[row].charAt(col) - '0';
-            queue.add(new int[] {row-1, col});
-            queue.add(new int[] {row, col-1});
-            queue.add(new int[] {row+1, col});
-            queue.add(new int[] {row, col+1});
+            for(int[] m : move) queue.add(new int[] {row+m[0], col+m[1]});
         }
         
-        return cnt;
+        if(food == 0) return -1;
+        return food;
     }
 }
