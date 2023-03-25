76fc8ff4f1ce1dd4045eba44a0a8328d30b3cefc
lsh4711 <120231876+lsh4711@users.noreply.github.com>
2023-03-25 22:04:58 +0900

    [unrated] Title: 숫자 카드 나누기, Time: 0.03 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/unrated/135807. 숫자 카드 나누기/숫자 카드 나누기.java
+++ 프로그래머스/unrated/135807. 숫자 카드 나누기/숫자 카드 나누기.java
@@ -1,32 +1,35 @@
-import java.util.*;
-
 class Solution {
     public int solution(int[] arrayA, int[] arrayB) {
-        int maxA = 0;
-        int maxB = 0;
-        int max = Math.max(arrayA[arrayA.length - 1], arrayB[arrayB.length - 1]);
+        int gcdA = 0;
+        int gcdB = 0;
         
-        label : for(int i = arrayA[0]; i > 1; i--) {
-            for(int a : arrayA) {
-                if(a % i != 0) continue label;
+        for(int a : arrayA) gcdA = gcd(gcdA, a);
+        for(int b : arrayB) gcdB = gcd(gcdB, b);
+        
+        for(int i = Math.max(gcdA, gcdB); i > 1; i--) {
+            label : if(gcdA % i == 0) {
+                for(int b : arrayB) {
+                    if(b % i == 0) break label;
+                }
+                return i;
             }
-            for(int b : arrayB) {
-                if(b % i == 0) continue label;
+            label : if(gcdB % i == 0) {
+                for(int a : arrayA) {
+                    if(a % i == 0) break label;
+                }
+                return i;
             }
-            maxA = i;
-            break;
-        }
-        label : for(int i = arrayB[0]; i > 1; i--) {
-            for(int b : arrayB) {
-                if(b % i != 0) continue label;
-            }
-            for(int a : arrayA) {
-                if(a % i == 0) continue label;
-            }
-            maxB = i;
-            break;
         }
         
-        return Math.max(maxA, maxB);
+        return 0;
+    }
+    static int gcd(int a, int b) {
+        while(b != 0) {
+            int r = a % b;
+            a = b;
+            b = r;
+        }
+        
+        return a;
     }
 }
