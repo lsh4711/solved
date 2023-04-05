3459c6292a4e474e4c17024c5f16a8c327857a31
lsh4711 <120231876+lsh4711@users.noreply.github.com>
2023-04-05 23:48:00 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.01 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.java
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -1,7 +1,7 @@
 class Solution {
     public int solution(int[] common) {
         int subtract = common[1] - common[0];
-        if(subtract == common[2]-common[1]) return common[common.length-1] + subtract;
-        return common[1] / common[0] * common[common.length-1];
+        if(subtract == common[2] - common[1]) return common[common.length - 1] + subtract;
+        return common[1] / common[0] * common[common.length - 1];
     }
 }
