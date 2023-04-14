3579493874322620d23b64a6449433255e2c5286
lsh4711 <120231876+lsh4711@users.noreply.github.com>
2023-04-15 02:37:44 +0900

    [Bronze V] Title: ??!, Time: 128 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Bronze/10926. ？？！/？？！.java
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -3,8 +3,7 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder().append(br.readLine()).append("??!");
 
-        System.out.println(sb);
+        System.out.println(new StringBuilder().append(br.readLine()).append("??!"));
     }
 }
