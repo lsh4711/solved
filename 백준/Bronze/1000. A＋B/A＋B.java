5b3116c935aa7f21affe3b0245ddcf907d8c6ee5
lsh4711 <120231876+lsh4711@users.noreply.github.com>
2023-04-15 02:02:44 +0900

    [Bronze V] Title: A+B, Time: 124 ms, Memory: 14188 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.java
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -1,15 +1,10 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(in.readLine());
-		int a = Integer.parseInt(st.nextToken());
-		int b = Integer.parseInt(st.nextToken());
-
-		System.out.println(a + b);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] nums = br.readLine().split(" ");
+        
+        System.out.println(Integer.parseInt(nums[0]) + Integer.parseInt(nums[1]));
+    }
 }
