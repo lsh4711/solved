06581ccef400de386f13c95ccd4becc40e77017e
lsh4711 <120231876+lsh4711@users.noreply.github.com>
2023-04-15 03:28:23 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 124 ms, Memory: 14128 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] nums = br.readLine().split(" ");
+        int A = Integer.parseInt(nums[0]);
+        int B = Integer.parseInt(nums[1]);
+
+        if(A < B) System.out.println("<");
+        else if(A > B) System.out.println(">");
+        else System.out.println("==");
+    }
+}
