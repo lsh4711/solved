1905c16cb27706e009adab2dc1a63a27aff36080
lsh4711 <120231876+lsh4711@users.noreply.github.com>
2023-04-15 02:30:14 +0900

    [Bronze V] Title: 사칙연산, Time: 136 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] nums = br.readLine().split(" ");
+        int A = Integer.parseInt(nums[0]);
+        int B = Integer.parseInt(nums[1]);
+        
+        System.out.println(A + B);
+        System.out.println(A - B);
+        System.out.println(A * B);
+        System.out.println(A / B);
+        System.out.println(A % B);
+    }
+}
