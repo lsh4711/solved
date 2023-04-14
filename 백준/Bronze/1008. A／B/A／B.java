167ee8c6e6e6988dcf04f5b6b1b715e9ce598eb1
lsh4711 <120231876+lsh4711@users.noreply.github.com>
2023-04-15 02:28:13 +0900

    [Bronze V] Title: A/B, Time: 128 ms, Memory: 14300 KB -BaekjoonHub

--- 백준/Bronze/1008. A／B/A／B.java
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -3,7 +3,6 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         String[] nums = br.readLine().split(" ");
         
         System.out.println(Integer.parseInt(nums[0]) / (double)Integer.parseInt(nums[1]));
