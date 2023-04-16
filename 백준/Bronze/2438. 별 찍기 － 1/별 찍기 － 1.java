cb4e473033d3c8e42fe9e0577953fe790b5c816b
lsh4711 <120231876+lsh4711@users.noreply.github.com>
2023-04-17 02:18:48 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 124 ms, Memory: 14360 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+
+        while(N-- != 0) System.out.println(sb.append('*'));
+    }
+}
