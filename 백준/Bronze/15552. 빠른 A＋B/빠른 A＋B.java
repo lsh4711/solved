601c6a737a2fa867d7b5a8aeb1d8a981bda1c72d
lsh4711 <120231876+lsh4711@users.noreply.github.com>
2023-04-17 01:53:44 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 952 ms, Memory: 280208 KB -BaekjoonHub

--- 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
@@ -1,16 +1,14 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
- 		StringTokenizer st = new StringTokenizer(br.readLine());
+        int T = Integer.parseInt(br.readLine());
 
-        int T = Integer.parseInt(st.nextToken());
-        for(int i=0; i<T; i++) {
-            st = new StringTokenizer(br.readLine());
-            sb.append(Integer.parseInt(st.nextToken())+Integer.parseInt(st.nextToken())).append("\n");
+        while(T-- != 0) {
+            String[] nums = br.readLine().split(" ");
+            sb.append(Integer.parseInt(nums[0]) + Integer.parseInt(nums[1])).append('\n');
         }
         
         System.out.println(sb);
