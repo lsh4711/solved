aaae6a29f470fc572b560d141425b0381b60c76e
lsh4711 <120231876+lsh4711@users.noreply.github.com>
2023-04-17 02:08:28 +0900

    [Bronze V] Title: 구구단, Time: 120 ms, Memory: 14112 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.java
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -3,8 +3,11 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         int num = br.readLine().charAt(0) - 48;
         
-        for(int i = 1; i <= 9; i++) System.out.println(String.format("%d * %d = %d", num, i, num * i));
+        for(int i = 1; i <= 9; i++) sb.append(num).append(" * ").append(i).append(" = ").append(num * i).append("\n");
+        
+        System.out.println(sb);
     }
 }
