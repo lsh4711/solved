bd6a96b5a2f40c4efa02ca8165a84e27306268d7
lsh4711 <120231876+lsh4711@users.noreply.github.com>
2023-04-17 01:29:54 +0900

    [Bronze V] Title: 합, Time: 132 ms, Memory: 14124 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+
+        System.out.println((num+1) * num / 2);
+    }
+}
