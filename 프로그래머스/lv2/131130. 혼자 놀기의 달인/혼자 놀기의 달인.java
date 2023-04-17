9d3a3cda336a352e727cf8f0525de9ad0dbe416e
lsh4711 <120231876+lsh4711@users.noreply.github.com>
2023-04-17 11:12:51 +0900

    [level 2] Title: 혼자 놀기의 달인, Time: 0.04 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int solution(int[] cards) {
+        int first = 0;
+        int[] groupCnt = new int[101];
+
+        for(int i = 0; i < cards.length; i++) {
+            if(cards[i] == 0) continue;
+            int cnt = 0;
+            for(int idx = i; cards[idx] != 0; cnt++) {
+                int temp = cards[idx] - 1;
+                cards[idx] = 0;
+                idx = temp;
+            }
+            groupCnt[cnt]++;
+        }
+
+        if(groupCnt[cards.length] != 0) return 0;
+        for(int i = groupCnt.length - 2; true; i--) {
+            if(groupCnt[i] == 0) continue;
+            if(first == 0) first = i;
+            else return first * i;
+            if(groupCnt[i] == 2) return first * i;
+        }
+    }
+}
