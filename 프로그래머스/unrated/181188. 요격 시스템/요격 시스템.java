5867536f1ffeba28073fc29a39ad381a3c558768
lsh4711 <120231876+lsh4711@users.noreply.github.com>
2023-04-18 08:12:57 +0900

    [unrated] Title: 요격 시스템, Time: 0.42 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181188. 요격 시스템/요격 시스템.java
+++ 프로그래머스/unrated/181188. 요격 시스템/요격 시스템.java
@@ -1,21 +1,19 @@
-import java.util.PriorityQueue;
+import java.util.*;
 
 class Solution {
     public int solution(int[][] targets) {
+        Arrays.sort(targets, (a, b) -> a[0] - b[0]); // x좌표 기준으로 정렬
         int cnt = 0;
-        PriorityQueue<int[]> pq = new PriorityQueue<>((arr1, arr2) -> arr1[1] - arr2[1]);
-        
-        for(int[] target : targets) pq.add(target);
-        while(!pq.isEmpty()) {
-            int[] target = pq.poll();
-            while(!pq.isEmpty()) {
-                int[] nextTarget = pq.peek();
-                if(target[1] <= nextTarget[0]) break;
-                pq.poll();
+        int last = -1;
+        for (int i = 0; i < targets.length; i++) {
+            int[] missile = targets[i];
+            if (missile[0] > last) { // 새로운 요격 미사일 필요
+                cnt++;
+                last = missile[1] - 1; // 해당 미사일로 커버되는 범위
+            } else if (missile[1] - 1 < last) { // 더 작은 범위로 커버 가능한 미사일 필요
+                last = missile[1] - 1; // 해당 미사일로 커버되는 범위
             }
-            cnt++;
         }
-        
         return cnt;
     }
 }
