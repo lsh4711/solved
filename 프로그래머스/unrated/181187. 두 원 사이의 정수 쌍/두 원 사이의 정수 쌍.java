697ca2a4fceeaaa7cf72f654bf5fc7c3e3aab656
lsh4711 <120231876+lsh4711@users.noreply.github.com>
2023-04-19 03:05:51 +0900

    [unrated] Title: 두 원 사이의 정수 쌍, Time: 9.50 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.java
+++ 프로그래머스/unrated/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.java
@@ -1,18 +1,12 @@
 class Solution {
     public long solution(int r1, int r2) {
-        return (checkPoint(r2, true) - checkPoint(r1, false)) * 4;
-    }
-    
-    static long checkPoint(int r, boolean b) {
-        long cnt = 0;
+        long point = 0;
         
-        for(long i = 0; i < r; i++) {
-            double sqrt = Math.sqrt((long)r * r - i * i);
-            long length = (long)sqrt;
-            if(!b && sqrt == length) length--;
-            cnt += length;
+        for(long i = 0; i < r2; i++) {
+            if(i < r1) point -= (long)Math.ceil(Math.sqrt((long)r1 * r1 - i * i)) - 1;
+            point += (long)Math.sqrt((long)r2 * r2 - i * i);
         }
         
-        return cnt;
+        return point * 4;
     }
-}  
+}        
