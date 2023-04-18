a28ae75dc6ff6ef953db1ec68c9a0ee786a05664
lsh4711 <120231876+lsh4711@users.noreply.github.com>
2023-04-19 03:18:24 +0900

    [unrated] Title: 추억 점수, Time: 0.03 ms, Memory: 70.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+
+class Solution {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        int[] scores = new int[photo.length];
+        HashMap<String, Integer> map = new HashMap<>();
+        
+        for(int i = 0; i < name.length; i++) map.put(name[i], yearning[i]);
+        for(int i = 0; i < photo.length; i++) {
+            int score = 0;
+            String[] pho = photo[i];
+            for(String p : pho) {
+                if(map.get(p) != null) score += map.get(p);
+            }
+            scores[i] = score;
+        }
+        
+        return scores;
+    }
+}
