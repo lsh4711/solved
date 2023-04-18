c9e57480c6876ffcdf32a2dc4d4556dae63642a5
lsh4711 <120231876+lsh4711@users.noreply.github.com>
2023-04-19 04:55:30 +0900

    [unrated] Title: 공원 산책, Time: 0.04 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/172928. 공원 산책/공원 산책.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int[] solution(String[] park, String[] routes) {
+        int[] start = new int[2];
+        int[][] ascii = new int[90][2];
+        char[] nswe = {'N', 'S', 'W', 'E'};
+        int[][] move = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        
+        for(int i = 0; i < 4; i++) ascii[nswe[i]] = move[i];
+        label : for(int i = 0; i < park.length; i++) {
+            for(int j = 0; j < park[0].length(); j++) {
+                if(park[i].charAt(j) != 'S') continue;
+                start = new int[] {i, j};
+                break label;
+            }
+        }
+        
+        label : for(String route : routes) {
+            int[] go = ascii[route.charAt(0)];
+            int distance = route.charAt(2) - 48;
+            int row = start[0];
+            int col = start[1];
+            for(int i = 0; i < distance; i++) {
+                int nextRow = row + go[0] ;
+                int nextCol = col + go[1];
+                if(nextRow < 0 || nextCol < 0 || nextRow >= park.length || nextCol >= park[0].length() || park[nextRow].charAt(nextCol) == 'X') continue label;
+                row = nextRow;
+                col = nextCol;
+            }
+            start = new int[] {row, col};
+        }
+        
+        return start;
+    }
+}
