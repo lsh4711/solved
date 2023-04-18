a58e209752ad9522be764c3ce82d6528013a19be
lsh4711 <120231876+lsh4711@users.noreply.github.com>
2023-04-19 05:01:09 +0900

    [unrated] Title: 공원 산책, Time: 0.05 ms, Memory: 70.5 MB -BaekjoonHub

--- 프로그래머스/unrated/172928. 공원 산책/공원 산책.java
+++ 프로그래머스/unrated/172928. 공원 산책/공원 산책.java
@@ -3,9 +3,9 @@ class Solution {
         int[] start = new int[2];
         int[][] ascii = new int[90][2];
         char[] nswe = {'N', 'S', 'W', 'E'};
-        int[][] move = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        int[][] moves = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
         
-        for(int i = 0; i < 4; i++) ascii[nswe[i]] = move[i];
+        for(int i = 0; i < 4; i++) ascii[nswe[i]] = moves[i];
         label : for(int i = 0; i < park.length; i++) {
             for(int j = 0; j < park[0].length(); j++) {
                 if(park[i].charAt(j) != 'S') continue;
@@ -15,13 +15,13 @@ class Solution {
         }
         
         label : for(String route : routes) {
-            int[] go = ascii[route.charAt(0)];
+            int[] move = ascii[route.charAt(0)];
             int distance = route.charAt(2) - 48;
             int row = start[0];
             int col = start[1];
             for(int i = 0; i < distance; i++) {
-                int nextRow = row + go[0] ;
-                int nextCol = col + go[1];
+                int nextRow = row + move[0] ;
+                int nextCol = col + move[1];
                 if(nextRow < 0 || nextCol < 0 || nextRow >= park.length || nextCol >= park[0].length() || park[nextRow].charAt(nextCol) == 'X') continue label;
                 row = nextRow;
                 col = nextCol;
