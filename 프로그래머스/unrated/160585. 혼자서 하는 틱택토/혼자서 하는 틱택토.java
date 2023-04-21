c6825497de901daa90e95c000c6a95acd332f6ce
lsh4711 <120231876+lsh4711@users.noreply.github.com>
2023-04-22 02:34:34 +0900

    [unrated] Title: 혼자서 하는 틱택토, Time: 8.79 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/unrated/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.java
+++ 프로그래머스/unrated/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.java
@@ -1,26 +1,25 @@
 class Solution {
     public int solution(String[] board) {
-        String O = "OOO";
-        String X = "XXX";
-        String diaLeft = "" + board[0].charAt(0) + board[1].charAt(1) + board[2].charAt(2);
-        String diaRight = "" + board[0].charAt(2) + board[1].charAt(1) + board[2].charAt(0);
+        String[] OX = {"OOO", "XXX"};
+        StringBuilder diaLeft = new StringBuilder();
+        StringBuilder diaRight = new StringBuilder();
         int[] cnt = new int[89];
         boolean[] isWin = new boolean[89];
         
-        for(String line : board) {
-            for(int i = 0; i < 3; i++) cnt[line.charAt(i)]++;
-        }
         for(int i = 0; i < 3; i++) {
+            for(int j = 0; j < 3; j++) cnt[board[i].charAt(j)]++;
             String rows = board[i];
             String cols = "" + board[0].charAt(i) + board[1].charAt(i) + board[2].charAt(i);
-            if(rows.equals(O) || rows.equals(X)) isWin[rows.charAt(0)] = true;
-            if(cols.equals(O) || cols.equals(X)) isWin[cols.charAt(0)] = true;
+            diaLeft.append(board[i].charAt(i));
+            diaRight.append(board[i].charAt(2 - i));
+            if(rows.equals(OX[0]) || cols.equals(OX[0])) isWin['O'] = true;
+            if(rows.equals(OX[1]) || cols.equals(OX[1])) isWin['X'] = true;
         }
-        if(diaLeft.equals(O) || diaLeft.equals(X)) isWin[diaLeft.charAt(0)] = true;
-        if(diaRight.equals(O) || diaRight.equals(X)) isWin[diaRight.charAt(0)] = true;        
+        if(diaLeft.toString().equals(OX[0]) || diaRight.toString().equals(OX[0])) isWin['O'] = true;
+        if(diaLeft.toString().equals(OX[1]) || diaRight.toString().equals(OX[1])) isWin['X'] = true;        
         
         if(cnt['O'] > cnt['X'] + 1 || cnt['O'] < cnt['X']) return 0;
         if(isWin['O'] && cnt['O'] == cnt['X'] || isWin['X'] && cnt['O'] != cnt['X']) return 0;
         return 1;
     }
-}
+} 
