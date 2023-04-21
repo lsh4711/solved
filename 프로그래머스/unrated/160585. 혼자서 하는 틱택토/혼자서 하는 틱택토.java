e45e8aa1200096e88a8d05334bed64ecc0f12094
lsh4711 <120231876+lsh4711@users.noreply.github.com>
2023-04-21 11:29:44 +0900

    [unrated] Title: 혼자서 하는 틱택토, Time: 9.29 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/unrated/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.java
+++ 프로그래머스/unrated/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.java
@@ -1,6 +1,26 @@
 class Solution {
     public int solution(String[] board) {
-        int answer = -1;
-        return answer;
+        String O = "OOO";
+        String X = "XXX";
+        String diaLeft = "" + board[0].charAt(0) + board[1].charAt(1) + board[2].charAt(2);
+        String diaRight = "" + board[0].charAt(2) + board[1].charAt(1) + board[2].charAt(0);
+        int[] cnt = new int[89];
+        boolean[] isWin = new boolean[89];
+        
+        for(String line : board) {
+            for(int i = 0; i < 3; i++) cnt[line.charAt(i)]++;
+        }
+        for(int i = 0; i < 3; i++) {
+            String rows = board[i];
+            String cols = "" + board[0].charAt(i) + board[1].charAt(i) + board[2].charAt(i);
+            if(rows.equals(O) || rows.equals(X)) isWin[rows.charAt(0)] = true;
+            if(cols.equals(O) || cols.equals(X)) isWin[cols.charAt(0)] = true;
+        }
+        if(diaLeft.equals(O) || diaLeft.equals(X)) isWin[diaLeft.charAt(0)] = true;
+        if(diaRight.equals(O) || diaRight.equals(X)) isWin[diaRight.charAt(0)] = true;        
+        
+        if(cnt['O'] > cnt['X'] + 1 || cnt['O'] < cnt['X']) return 0;
+        if(isWin['O'] && cnt['O'] == cnt['X'] || isWin['X'] && cnt['O'] != cnt['X']) return 0;
+        return 1;
     }
 }
