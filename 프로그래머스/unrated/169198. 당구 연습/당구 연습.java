2b93efb1d3546f24f775cdfe31dd270764bfd336
lsh4711 <120231876+lsh4711@users.noreply.github.com>
2023-04-24 10:54:58 +0900

    [unrated] Title: 당구 연습, Time: 0.71 ms, Memory: 71.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/169198. 당구 연습/당구 연습.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] solution(int m, int n, int startX, int startY, int[][] balls) {
+        int[] answer = new int[balls.length];
+        
+        for(int i = 0; i < balls.length; i++) answer[i] = cushion(balls[i], m, n, startX, startY);
+        
+        return answer;
+    }
+    
+    static int cushion(int[] ball, int m, int n, int startX, int startY) {
+        int result = (int)5e6;
+        int[][] dirs = {{startX - ball[0], n - startY, n - ball[1]},
+                {startX - ball[0], startY, ball[1]},
+                {startY - ball[1], startX, ball[0]},
+                {startY - ball[1], m - startX, m - ball[0]}};
+        
+        for(int i = 0; i < 4; i++) {
+            int[] dir = dirs[i];
+            if(dir[0] == 0 && dir[1] > dir[2]) continue;
+            int length = (int)(Math.pow(dir[0], 2) + (int)Math.pow(dir[1] + dir[2], 2));
+            if(result > length) result = length;
+        }
+        
+        return result;
+    }
+}
