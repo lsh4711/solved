900adcabe6ae7099b98b30f58ad387d42d084cd3
lsh4711 <120231876+lsh4711@users.noreply.github.com>
2023-04-27 10:29:38 +0900

    [level 2] Title: N-Queen, Time: 94.62 ms, Memory: 76.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12952. N－Queen/N－Queen.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int n) {
+        return dfs(new boolean[3][34], n, 0);
+    }
+    
+    static int dfs(boolean[][] visits, int n, int row) {
+        if(n == row) return 1;
+        int cnt = 0;
+        
+        for(int col = 0; col < n; col++) {
+            if(visits[0][col] || visits[1][row + col] || visits[2][row - col + 11]) continue;
+            visits[0][col] = true; visits[1][row + col] = true; visits[2][row - col + 11] = true;
+            cnt += dfs(visits, n, row + 1);
+            visits[0][col] = false; visits[1][row + col] = false; visits[2][row - col + 11] = false;
+        }
+        
+        return cnt;
+    }
+}
