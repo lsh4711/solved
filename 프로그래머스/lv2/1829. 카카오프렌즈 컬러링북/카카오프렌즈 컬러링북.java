34a37f9450f2e0986d3819c7873498f607c11de3
lsh4711 <120231876+lsh4711@users.noreply.github.com>
2023-05-01 10:08:48 +0900

    [level 2] Title: 카카오프렌즈 컬러링북, Time: 16.65 ms, Memory: 116 MB -BaekjoonHub

--- 프로그래머스/lv2/1829. 카카오프렌즈 컬러링북/카카오프렌즈 컬러링북.java
+++ 프로그래머스/lv2/1829. 카카오프렌즈 컬러링북/카카오프렌즈 컬러링북.java
@@ -2,15 +2,12 @@ class Solution {
     public int[] solution(int m, int n, int[][] picture) {
         int cnt = 0;
         int max = 0;
-        int[][] copy = new int[m][n];
-        
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) copy[i][j] = picture[i][j];
-        }
+        boolean[][] visits = new boolean[m][n];
+
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
-                if(copy[i][j] == 0) continue;
-                max = Math.max(max, dfs(copy, new int[] {i, j}, m, n));
+                if(visits[i][j] || picture[i][j] == 0) continue;
+                max = Math.max(max, dfs(picture, visits, new int[] {i, j}, m, n));
                 cnt++;
             }
         }
@@ -18,19 +15,18 @@ class Solution {
         return new int[] {cnt, max};
     }
 
-    static int dfs(int[][] picture, int[] xy, int m, int n) {
-        int tmp = picture[xy[0]][xy[1]];
-        if(tmp == 0) return 0;
+    static int dfs(int[][] picture, boolean[][] visits, int[] xy, int m, int n) {
+        if(visits[xy[0]][xy[1]]) return 0;
+        visits[xy[0]][xy[1]] = true;
         int cnt = 1;
         int[][] moves = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-        
-        picture[xy[0]][xy[1]] = 0;
+
         for(int i = 0; i < 4; i++) {
             int[] move = moves[i];
             int[] next = {xy[0] + move[0], xy[1] + move[1]};
-            if(next[0] < 0 || next[1] < 0 || next[0] >= m || next[1] >= n) continue;
-            if(picture[next[0]][next[1]] != tmp) continue;
-            cnt += dfs(picture, next, m, n);
+            if(next[0] < 0 || next[1] < 0 || next[0] >= m || next[1] >= n || visits[next[0]][next[1]]) continue;
+            if(picture[xy[0]][xy[1]] != picture[next[0]][next[1]] || picture[next[0]][next[1]] == 0) continue;
+            cnt += dfs(picture, visits, next, m, n);
         }
 
         return cnt;
