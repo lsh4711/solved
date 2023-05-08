3d309b26756c61bbc04cf64aa034adacd9144138
lsh4711 <120231876+lsh4711@users.noreply.github.com>
2023-05-08 11:18:05 +0900

    [level 2] Title: 숫자 블록, Time: 102.36 ms, Memory: 53.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12923. 숫자 블록/숫자 블록.java
+++ 프로그래머스/lv2/12923. 숫자 블록/숫자 블록.java
@@ -1,14 +1,11 @@
 class Solution {
     public int[] solution(long begin, long end) {
-        int[] result = new int[(int)(end - begin) + 1];
+        int[] road = new int[(int)(end - begin) + 1];
         
         for(int i = 1; i <= Math.min(end / 2, 10000000); i++) {
-            for(int j = (int)Math.max((begin / i) * i, i + i); j <= end; j += i) {
-                if(j < begin) continue;
-                result[j - (int)begin] = i;
-            }
+            for(int j = (int)Math.max((begin - 1) / i + 1, 2) * i; j <= end; j += i) road[j - (int)begin] = i;
         }
         
-        return result;
+        return road;
     }
-}  
+}             
