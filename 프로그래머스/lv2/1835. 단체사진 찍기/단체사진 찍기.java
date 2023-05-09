a837dfbea96bcdd171ae0f559d1b1c128ab444a9
lsh4711 <120231876+lsh4711@users.noreply.github.com>
2023-05-09 12:40:18 +0900

    [level 2] Title: 단체사진 찍기, Time: 1556.37 ms, Memory: 399 MB -BaekjoonHub

--- 프로그래머스/lv2/1835. 단체사진 찍기/단체사진 찍기.java
+++ 프로그래머스/lv2/1835. 단체사진 찍기/단체사진 찍기.java
@@ -1,10 +1,13 @@
 class Solution {
     public int solution(int n, String[] data) {
-        return dfs(new char[] {'A', 'C', 'F', 'J', 'M', 'N', 'R', 'T'}, data, new boolean[8], "");
+        // 모든 조합을 검증하는 방법으로 접근
+        char[] friends = {'A', 'C', 'F', 'J', 'M', 'N', 'R', 'T'};
+            
+        return dfs(friends, data, new boolean[8], "");
     }
     
     static int dfs(char[] friends, String[] data, boolean[] visits, String str) {
-        if(str.length() == 8) return check(data, str);
+        if(str.length() == 8) return check(data, str); // 8명이 나열되었을떄 check로 검증하고 0 | 1 리턴
         int cnt = 0;
         
         for(int i = 0; i < 8; i++) {
@@ -19,31 +22,31 @@ class Solution {
     
     static int check(String[] data, String str) {
         label : for(String d : data) {
-            char me = d.charAt(0);
-            char you = d.charAt(2);
-            char op = d.charAt(3);
+            char me = d.charAt(0); // 조건 제시한 친구
+            char you = d.charAt(2); // 상대방
+            char op = d.charAt(3); // 연산자
             int start = 1;
-            int end = d.charAt(4) - 47;
+            int end = d.charAt(4) - 47; // -47 = (- '0' + 1)
             int meIdx = -1;
             
-            if(op == '=') start = end;
-            else if(op == '<') end--;
-            else {
+            if(op == '=') start = end; // 간격 = 2 -> start = 3
+            else if(op == '<') end--;  // 간격 < 2 -> end = 2
+            else {                                  // 간격 > 2 -> start = 4
                 start = end + 1;
                 end = 7;
             }
-            for(int i = 0; i < 8; i++) {
+            for(int i = 0; i < 8; i++) { // 조건 제시한 친구 위치 탐색
                 if(str.charAt(i) != me) continue;
                 meIdx = i;
                 break;
             }
-            for(int i = start; i <= end; i++) {
+            for(int i = start; i <= end; i++) { // 조건 제시한 친구 좌우로 조건 만족하는지 검증
                 if(meIdx - i >= 0 && str.charAt(meIdx - i) == you) continue label;
                 if(meIdx + i < 8 && str.charAt(meIdx + i) == you) continue label;
             }
-            return 0;
+            return 0; // continue label이 실행 안되고 도달했을 때 == 조건 충족 실패
         }
         
-        return 1;
+        return 1; // 여기까지 도달 == 조건이 최소 하나라도 만족
     }
 }
