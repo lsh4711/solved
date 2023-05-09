dbfdacb2aaf046c07ea28df8d414d7371b4b5fec
lsh4711 <120231876+lsh4711@users.noreply.github.com>
2023-05-09 15:35:09 +0900

    [unrated] Title: 혼자서 하는 틱택토, Time: 0.04 ms, Memory: 84.3 MB -BaekjoonHub

--- 프로그래머스/unrated/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.java
+++ 프로그래머스/unrated/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.java
@@ -1,22 +1,24 @@
 class Solution {
     public int solution(String[] board) {
-        String[] OX = {"OOO", "XXX"};
-        StringBuilder diaLeft = new StringBuilder();
-        StringBuilder diaRight = new StringBuilder();
         int[] cnt = new int[89];
         boolean[] isWin = new boolean[89];
         
         for(int i = 0; i < 3; i++) {
-            for(int j = 0; j < 3; j++) cnt[board[i].charAt(j)]++;
-            String rows = board[i];
-            String cols = "" + board[0].charAt(i) + board[1].charAt(i) + board[2].charAt(i);
-            diaLeft.append(board[i].charAt(i));
-            diaRight.append(board[i].charAt(2 - i));
-            if(rows.equals(OX[0]) || cols.equals(OX[0])) isWin['O'] = true;
-            else if(rows.equals(OX[1]) || cols.equals(OX[1])) isWin['X'] = true;
+            int[] line = new int[4];
+            for(int j = 0; j < 3; j++) {
+                if(board[i].charAt(j) == 'O') line[0]++;
+                else if(board[i].charAt(j) == 'X') line[0]--;
+                if(board[j].charAt(i) == 'O') line[1]++;
+                else if(board[j].charAt(i) == 'X') line[1]--;
+                if(board[j].charAt(j) == 'O') line[2]++;
+                else if(board[j].charAt(j) == 'X') line[2]--;
+                if(board[j].charAt(2 - j) == 'O') line[3]++;
+                else if(board[j].charAt(2 - j) == 'X') line[3]--;
+                cnt[board[i].charAt(j)]++;
+            }
+            if(line[0] == 3 || line[1] == 3 || line[2] == 3 || line[3] == 3) isWin['O'] = true;
+            else if(line[0] == -3 || line[1] == -3 || line[2] == -3 || line[3] == -3) isWin['X'] = true;
         }
-        if(diaLeft.toString().equals(OX[0]) || diaRight.toString().equals(OX[0])) isWin['O'] = true;
-        else if(diaLeft.toString().equals(OX[1]) || diaRight.toString().equals(OX[1])) isWin['X'] = true;        
         
         if(cnt['O'] > cnt['X'] + 1 || cnt['O'] < cnt['X']) return 0;
         if(isWin['O'] && cnt['O'] == cnt['X'] || isWin['X'] && cnt['O'] != cnt['X']) return 0;
