ac615aa4197f2b6616f094c579f3c07c885e0da9
lsh4711 <120231876+lsh4711@users.noreply.github.com>
2023-05-09 13:41:04 +0900

    [unrated] Title: 혼자서 하는 틱택토, Time: 9.16 ms, Memory: 82.7 MB -BaekjoonHub

--- 프로그래머스/unrated/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.java
+++ 프로그래머스/unrated/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.java
@@ -1,65 +1,25 @@
 class Solution {
     public int solution(String[] board) {
-        int oCount = 0;
-        int[] row  = new int[3]; // 가로
-        int[] col = new int[3];  // 세로
-        int[] diagonal = new int[3]; // 대각선
-        boolean oWin = false;
-        boolean xWin = false;
-
-        for(int i=0; i< board.length;i++){
-            for(int j=0; j<board[i].length();j++){
-                char ch = board[i].charAt(j);
-                int value = 0;
-
-                if(ch=='O'){
-                    value = 1;
-                }
-
-                if(ch=='X'){
-                    value = -1;
-                }
-
-                row[i] += value;
-                col[j] += value;
-                oCount += value;
-                if(i==j){
-                    diagonal[0] += value;
-                }
-                if(i+j==2){
-                    diagonal[1] += value;
-                }
-            }
+        String[] OX = {"OOO", "XXX"};
+        StringBuilder diaLeft = new StringBuilder();
+        StringBuilder diaRight = new StringBuilder();
+        int[] cnt = new int[89];
+        boolean[] isWin = new boolean[89];
+        
+        for(int i = 0; i < 3; i++) {
+            for(int j = 0; j < 3; j++) cnt[board[i].charAt(j)]++;
+            String rows = board[i];
+            String cols = "" + board[0].charAt(i) + board[1].charAt(i) + board[2].charAt(i);
+            diaLeft.append(board[i].charAt(i));
+            diaRight.append(board[i].charAt(2 - i));
+            if(rows.equals(OX[0]) || cols.equals(OX[0])) isWin['O'] = true;
+            else if(rows.equals(OX[1]) || cols.equals(OX[1])) isWin['X'] = true;
         }
-
-        for(int i=0; i<3;i++){
-            if(row[i]== 3 || col[i] == 3 || diagonal[i] == 3 ){
-                oWin = true;
-            }
-            if(row[i]== -3 || col[i] == -3 || diagonal[i] == -3){
-                xWin = true;
-            }
-        }
-
-
-        if(!oWin && !xWin){  //둘 다 못이긴다. Ocount 0 or 1
-            if(oCount == 0 || oCount == 1){
-                return 1;
-            }
-        }
-
-        if(xWin && !oWin){ // X가 이긴다  oCount = 0
-            if(oCount==0){
-                return 1;
-            }
-        }
-
-        if(oWin && !xWin){ // O가 이긴다  oCount =  1
-            if(oCount==1){
-                return 1;
-            }
-        }
-
-        return 0;
+        if(diaLeft.toString().equals(OX[0]) || diaRight.toString().equals(OX[0])) isWin['O'] = true;
+        else if(diaLeft.toString().equals(OX[1]) || diaRight.toString().equals(OX[1])) isWin['X'] = true;        
+        
+        if(cnt['O'] > cnt['X'] + 1 || cnt['O'] < cnt['X']) return 0;
+        if(isWin['O'] && cnt['O'] == cnt['X'] || isWin['X'] && cnt['O'] != cnt['X']) return 0;
+        return 1;
     }
 }
