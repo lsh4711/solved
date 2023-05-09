91296884f5f7974f4f901daaa6d9143244fc4c0a
lsh4711 <120231876+lsh4711@users.noreply.github.com>
2023-05-09 13:30:26 +0900

    [level 2] Title: 단체사진 찍기, Time: 1316.78 ms, Memory: 393 MB -BaekjoonHub

--- 프로그래머스/lv2/1835. 단체사진 찍기/단체사진 찍기.java
+++ 프로그래머스/lv2/1835. 단체사진 찍기/단체사진 찍기.java
@@ -18,29 +18,21 @@ class Solution {
     }
     
     static int check(String[] data, String str) {
-        label : for(String d : data) {
+        for(String d : data) {
             char me = d.charAt(0);
             char you = d.charAt(2);
             char op = d.charAt(3);
-            int start = 1;
-            int end = d.charAt(4) - 47;
+            int distance = d.charAt(4) - '0';
             int meIdx = -1;
-            
-            if(op == '=') start = end;
-            else if(op == '<') end--;
-            else {
-                start = end + 1;
-                end = 7;
-            }
+            int youIdx = -1;
             for(int i = 0; i < 8; i++) {
-                if(str.charAt(i) != me) continue;
-                meIdx = i;
-                break;
+                if(str.charAt(i) == me) meIdx = i;
+                else if(str.charAt(i) == you) youIdx = i;
             }
-            for(int i = start; i <= end; i++) {
-                if(meIdx - i >= 0 && str.charAt(meIdx - i) == you) continue label;
-                if(meIdx + i < 8 && str.charAt(meIdx + i) == you) continue label;
-            }
+            int abs = Math.abs(meIdx - youIdx);
+            if(op == '<' && abs <= distance) continue;
+            if(op == '>' && abs > distance + 1) continue;
+            if(op == '=' && abs == distance + 1) continue;
             return 0;
         }
         
