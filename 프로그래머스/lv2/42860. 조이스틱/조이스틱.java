835fcb47cd7912c97f3ce9c268b18ef8a139d49f
lsh4711 <120231876+lsh4711@users.noreply.github.com>
2023-05-14 17:20:35 +0900

    [level 2] Title: 조이스틱, Time: 0.06 ms, Memory: 76.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42860. 조이스틱/조이스틱.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int solution(String name) {
+        int dir = 0;
+        int cnt = 0;
+        int tmp = 0;
+        int start = 0;
+        int[] lineA = {0, 0, 0};
+        char[] chars = name.toCharArray();
+        
+        for(int i = 0; i < name.length(); i++) {
+            if(chars[i] == 'A') tmp++;
+            else tmp = 0;
+            if(lineA[2] < tmp) lineA = new int[] {i - tmp + 1, i, tmp};
+        }
+        if(lineA[0] <= 1) dir = -1;
+        else if(lineA[1] == chars.length - 1) dir = 1;
+        else if(lineA[0] > chars.length - lineA[1]) dir = -1;
+        else dir = 1;
+        while(true) {
+            int interval = 0;
+            char now = chars[start];
+            cnt += Math.min(now - 65, 91 - now);
+            chars[start] = 'A';
+            tmp = start;
+            while(start == tmp) {
+                if(++interval == chars.length) return cnt;
+                int left = (start + chars.length - interval) % chars.length;
+                int right = (start + interval) % chars.length;
+                if(dir <= 0 && chars[left] != 'A') start = left;
+                else if(dir >= 0 && chars[right] != 'A') start = right;
+            }
+            cnt += interval;
+            dir = 0;
+        }
+    }
+}
