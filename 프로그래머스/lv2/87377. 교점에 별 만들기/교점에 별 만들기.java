8ea445a6d88c73b7140aff626bae28a437da3649
lsh4711 <120231876+lsh4711@users.noreply.github.com>
2023-05-17 19:08:33 +0900

    [level 2] Title: 교점에 별 만들기, Time: 0.04 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/lv2/87377. 교점에 별 만들기/교점에 별 만들기.java
+++ 프로그래머스/lv2/87377. 교점에 별 만들기/교점에 별 만들기.java
@@ -1,51 +1,45 @@
 import java.util.*;
 
 class Solution {
+    static ArrayList<int[]> list = new ArrayList<>();
     public String[] solution(int[][] line) {
-        ArrayList<int[]> list = new ArrayList<>();
+        int min = Integer.MIN_VALUE;
+        int max = Integer.MAX_VALUE;
+        int[] xRange = {max, min};
+        int[] yRange = {max, min};
 
         for(int i = 0; i < line.length - 1; i++) {
             int[] line1 = line[i];
             for(int j = i + 1; j < line.length; j++) {
                 int[] line2 = line[j];
-                long tmp1 = line1[0];
-                long tmp2 = line2[0];
-                long[] cross = new long[3];
-                int[] copy = (line1[0] != 0) ? line1 : line2; // 0일수도 있음
-                for(int k = 1; k < 3; k++) cross[k] = line1[k] * tmp2 - line2[k] * tmp1;
-                if(cross[1] == 0) continue; // 평행할 때
-                int x;
-                int y;
-                if(cross[2] % cross[1] != 0) continue; // 반대로도 나눠봐야함
-                else y = (int)(cross[2] / -cross[1]);
-                tmp1 = copy[2] + copy[1] * y;
-                if(tmp1 % copy[0] != 0) continue;
-                else x = (int)(tmp1 / -copy[0]);
+                long denom = (long)line1[0] * line2[1] - (long)line1[1] * line2[0];
+                long xNum = (long)line1[1] * line2[2] - (long)line1[2] * line2[1];
+                long yNum = (long)line1[2] * line2[0] - (long)line1[0] * line2[2];
+                if(denom == 0 || xNum % denom != 0 || yNum % denom != 0) continue;
+                int x = (int)(xNum / denom);
+                int y = (int)(yNum / denom);
+                if(xRange[0] > x) xRange[0] = x;
+                if(xRange[1] < x) xRange[1] = x;
+                if(yRange[0] > y) yRange[0] = y;
+                if(yRange[1] < y) yRange[1] = y;
                 list.add(new int[] {x, y});
             }
         }
-        int[] tmp = list.get(0);
-        int[] x = {tmp[0], tmp[0]};
-        int[] y = {tmp[1], tmp[1]};
-        for(int[] arr : list) {
-            if(arr[0] < x[0]) x[0] = arr[0];
-            if(arr[0] > x[1]) x[1] = arr[0];
-            if(arr[1] < y[0]) y[0] = arr[1];
-            if(arr[1] > y[1]) y[1] = arr[1];
+        boolean[][] coorPlane = new boolean[yRange[1] - yRange[0] + 1][xRange[1] - xRange[0] + 1];
+        String[] result = new String[coorPlane.length];
+        
+        for(int[] xy : list) {
+            int xCal = xy[0] - xRange[0];
+            int yCal = coorPlane.length - (xy[1] - yRange[0] + 1);
+            coorPlane[yCal][xCal] = true;
         }
-        String[] result = new String[y[1] - y[0] + 1];
-        int idx = 0;
-        for(int i = y[1]; i >= y[0]; i--) {
+        for(int i = 0; i < coorPlane.length; i++) {
             StringBuilder sb = new StringBuilder();
-            label: for(int j = x[0]; j <= x[1]; j++) {
-                for(int[] arr : list) {
-                    if(arr[0] != j || arr[1] != i) continue;
-                    sb.append('*');
-                    continue label;
-                }
-                sb.append('.');
+            for(int j = 0; j < coorPlane[0].length; j++) {
+                if(coorPlane[i][j]) sb.append('*');
+                else sb.append('.');
             }
-            result[idx++] = sb.toString();
+            result[i] = sb.toString();
         }
 
         return result;
