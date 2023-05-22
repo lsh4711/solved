de473b1a5f81c742dc85882e1ce9df03fab559ba
lsh4711 <120231876+lsh4711@users.noreply.github.com>
2023-05-22 10:34:15 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.17 ms, Memory: 88.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] operations) {
+        int[] result = new int[2];
+        LinkedList<Integer> list = new LinkedList<>();
+        
+        for (String operation : operations) {
+            char oper = operation.charAt(0);
+            int num = Integer.parseInt(operation.substring(2, operation.length()));
+            
+            if (oper == 'I') {
+                insert(list, num);
+            } else if (list.size() == 0) {
+                continue;
+            } else if (num < 0) {
+                list.remove(0);
+            } else {
+                list.remove(list.size() - 1);
+            }
+        }
+        if (list.size() > 1) result = new int[] {list.get(list.size() - 1), list.get(0)};
+        
+        return result;
+    }
+    
+    static void insert(LinkedList<Integer> list, int num) {
+        if (list.size() == 0) {
+            list.add(num);
+            return;
+        }
+        
+        for (int i = 0; i < list.size(); i++) {
+            if(list.get(i) < num) continue;
+            list.add(i, num);
+            return;
+        }
+        
+        list.add(num);
+    }
+}
