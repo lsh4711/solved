2f4efa7115985283ba311440e8a9e5631455d975
lsh4711 <120231876+lsh4711@users.noreply.github.com>
2023-05-24 13:26:37 +0900

    [unrated] Title: 이모티콘 할인행사, Time: 0.04 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/150368. 이모티콘 할인행사/이모티콘 할인행사.java
@@ -0,0 +1,54 @@
+class Solution {
+    static int[] result = new int[2];
+    
+    public int[] solution(int[][] users, int[] emoticons) {
+        dfs(users, emoticons, new int[emoticons.length][2], 0);
+        
+        return result;
+    }
+    
+    static void dfs(int[][] users, int[] emoticons, int[][] prices, int idx) {
+        if (prices.length == idx) {
+            check(users, prices);
+            return;
+        }
+        
+        for (int i = 0; i < 4; i++) {
+            int discntRate = (i + 1) * 10;
+            int price = emoticons[idx] * (9 - i) / 10;
+            prices[idx][0] = discntRate;
+            prices[idx][1] = price;
+            dfs(users, emoticons, prices, idx + 1);
+        }
+        
+    }
+    
+    static void check(int[][] users, int[][] prices) {
+        int plus = 0;
+        int income = 0;
+        
+        for (int i = 0; i < users.length; i++) {
+            int total = 0;
+            int expected  = users[i][0];
+            for (int j = 0; j < prices.length; j++) {
+                int actual  = prices[j][0];
+                int price = prices[j][1];
+                if (expected <= actual) {
+                    total += price;
+                }
+            }
+            if (users[i][1] <= total) {
+                plus++;
+            } else {
+                income += total;
+            }
+        }
+        if (result[0] < plus) {
+                result[0] = plus;
+                result[1] = income;
+        } else if (result[0] == plus && result[1] < income) {
+            result[1] = income;
+        }
+        
+    }
+}
