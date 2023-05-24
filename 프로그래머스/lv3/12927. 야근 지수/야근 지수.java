f86e9ed3dd37f463ea802c3cd8342342a5e21adf
lsh4711 <120231876+lsh4711@users.noreply.github.com>
2023-05-24 12:21:12 +0900

    [level 3] Title: 야근 지수, Time: 35.80 ms, Memory: 53 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/12927. 야근 지수/야근 지수.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+
+class Solution {
+    public long solution(int n, int[] works) {
+        long result = 0;
+        int idx = works.length - 1;
+        
+        Arrays.sort(works);
+        while (n > 0) {
+            int target = idx;
+            int before = works[idx - 1];
+            int now = works[idx];
+            
+            if (before > now) {
+                if (idx > 1) {
+                    idx--;
+                    continue;
+                }
+                target--;
+            } else if (idx < works.length - 1 && now <= works[idx + 1]) {
+                idx++;
+                continue;
+            }
+            if (works[target] == 0) {
+                break;
+            }
+            works[target]--;
+            n--;
+        }
+        
+        for (int work : works) {
+            result += work * work;
+        }
+        
+        return result;
+    }
+}   
