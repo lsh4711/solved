cf4ade8082d2665b9fa680a706a6c0cc54c5922c
lsh4711 <120231876+lsh4711@users.noreply.github.com>
2023-05-25 10:57:26 +0900

    [level 3] Title: 최고의 집합, Time: 0.01 ms, Memory: 52.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/12938. 최고의 집합/최고의 집합.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] solution(int n, int s) {
+        if (n > s) return new int[] {-1};
+        int idx = 0;
+        int splitN = s / n;
+        int[] result = new int[n];
+        
+        while(n-- > 0) {
+            if (s % (splitN + 1) == 0 && s / (splitN + 1) == result.length - idx) {
+                splitN++;
+            }
+            result[idx++] = splitN;
+            s -= splitN;
+        }
+        
+        return result;
+    }
+}
