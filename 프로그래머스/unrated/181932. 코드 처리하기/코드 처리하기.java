d6b308a5536117f84cbf3c72fcf82c41ab69ae1c
lsh4711 <120231876+lsh4711@users.noreply.github.com>
2023-05-26 15:10:10 +0900

    [unrated] Title: 코드 처리하기, Time: 0.06 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String solution(String code) {
+        int mode = 0;
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 0; i < code.length(); i++) {
+            char c = code.charAt(i);
+            if (mode == 0) {
+                if (c != '1' && i % 2 == 0) {
+                    sb.append(c);
+                } else if (c == '1'){
+                    mode = 1;
+                }
+            } else { // mode가 1 일 때
+                if (c != '1' && i % 2 == 1) {
+                    sb.append(c);
+                } else if (c == '1') {
+                    mode = 0;
+                }
+            }
+        }
+        
+        if (sb.length() == 0) {
+            return "EMPTY";
+        }
+        return sb.toString();
+    }
+}
