4868b5dda9badb80a21a79370d0fd2cfabe8e7b8
lsh4711 <120231876+lsh4711@users.noreply.github.com>
2023-06-05 21:32:06 +0900

    [level 3] Title: 베스트앨범, Time: 0.76 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.java
@@ -1,6 +1,56 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 class Solution {
-    public int[] solution(String[] genres, int[] plays) {
-        int[] answer = {};
-        return answer;
+    public ArrayList<Integer> solution(String[] genres, int[] plays) {
+        HashMap<String, Integer> map = new HashMap<>();
+        ArrayList<int[]> datas = new ArrayList<>();
+        
+        int cnt = 0;
+        
+        for (int i = 0; i < genres.length; i++) {
+            String genre = genres[i];
+            int play = plays[i];
+            if (map.get(genre) == null) {
+                map.put(genre, datas.size());
+                datas.add(new int[] {0, 0, 0, 0, 0});
+            }
+            int idx = map.get(genre);
+            add(datas.get(idx), i, play);
+        }
+        datas.sort((arr1, arr2) -> arr2[0] - arr1[0]);
+
+        ArrayList<Integer> results = new ArrayList<>();
+        int idx = 0;
+        
+        for (int[] data : datas) {
+            System.out.println(java.util.Arrays.toString(data));
+            results.add(data[1]);
+            if (data[4] != 0) {
+                results.add(data[3]);
+            }
+        }
+        
+        return results;
+    }
+    
+    static int add(int[] value, int idx, int play) {
+        int cnt = 0;
+        
+        value[0] += play;
+        if (value[2] < play) {
+            int tmpIdx = idx;
+            int tmpPlay = play;
+            idx = value[1];
+            play = value[2];
+            value[1] = tmpIdx;
+            value[2] = tmpPlay;
+        }
+        if (value[4] < play) {
+            value[3] = idx;
+            value[4] = play;
+        }
+        
+        return cnt;
     }
 }
