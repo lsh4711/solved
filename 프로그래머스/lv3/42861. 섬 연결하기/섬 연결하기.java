59948ee425c2c3fb519d1c74d0f73d4e6f010ff9
lsh4711 <120231876+lsh4711@users.noreply.github.com>
2023-07-02 14:38:03 +0900

    [level 3] Title: 섬 연결하기, Time: 1.80 ms, Memory: 80.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42861. 섬 연결하기/섬 연결하기.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+
+class Solution {
+    static PriorityQueue<int[]> pq = new PriorityQueue<>((a1, a2) -> a1[2] - a2[2]);
+    
+    public int solution(int n, int[][] costs) {
+        ArrayList<int[]>[]  routes = new ArrayList[n];
+        
+        for (int i = 0; i < n; i++) {
+            routes[i] = new ArrayList<>();
+        }
+        for (int i = 0; i < costs.length; i++) {
+            int[] cost = costs[i];
+            int a = cost[0];
+            int b = cost[1];
+            int c = cost[2]; 
+            routes[a].add(new int[] {b, c});
+            routes[b].add(new int[] {a, c});
+        }
+        for (int[] route : routes[0]) {
+            pq.add(new int[] {0, route[0], route[1]});
+        }
+        boolean[] visits = new boolean[n];
+        visits[0] = true;
+        
+        int totalDistance = dijkstra(routes, visits);
+        
+        return totalDistance;
+    }
+    
+    static int dijkstra(ArrayList<int[]>[] routes, boolean[] visits) {
+        int totalDistance = 0;
+        
+        while (!pq.isEmpty()) {
+            int[] now = pq.poll();
+            if (visits[now[1]]) {
+                continue;
+            }
+            for (int[] route : routes[now[1]]) {
+                pq.add(new int[] {now[1], route[0], route[1]});
+            }
+            System.out.printf("a: %d, b: %d, c: %d\n", now[0], now[1], now[2]);
+            visits[now[1]] = true;
+            totalDistance += now[2];
+        }
+        
+        return totalDistance;
+    }
+}
