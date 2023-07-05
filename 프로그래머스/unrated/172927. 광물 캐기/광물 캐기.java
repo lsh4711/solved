55fa6803f89099b2524d2b3dca4930d33ccb918c
lsh4711 <120231876+lsh4711@users.noreply.github.com>
2023-07-05 16:42:01 +0900

    [unrated] Title: 광물 캐기, Time: 0.59 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/unrated/172927. 광물 캐기/광물 캐기.java
+++ 프로그래머스/unrated/172927. 광물 캐기/광물 캐기.java
@@ -4,31 +4,31 @@ class Solution {
     public int solution(int[] picks, String[] minerals) {
         int result = 0;
         int pickCount = picks[0] + picks[1] + picks[2];
-        int min = Math.min((minerals.length + 4) / 5, pickCount);
-        int[][] stacks = new int[min][3];
+        int stackSize = Math.min((minerals.length + 4) / 5, pickCount);
+        int[][] stacks = new int[stackSize][3];
         
         for (int i = 0; i < minerals.length; i++) {
             int idx = i / 5;
-            if (idx == min) {
+            if (idx == stackSize) {
                 break;
             }
-            String mineral = minerals[i];
-            if (mineral.length() == 7) {
+            int mineralLength = minerals[i].length();
+            if (mineralLength == 7) {
                 stacks[idx][0]++;
-            } else if (mineral.length() == 4) {
+            } else if (mineralLength == 4) {
                 stacks[idx][1]++;
             } else {
                 stacks[idx][2]++;
             }
         }
         Arrays.sort(stacks, (a1, a2) -> {
-            if (a1[0] == a2[0]) {
-                if (a1[1] == a2[1]) {
-                    return a2[2] - a1[2];
-                }
+            if (a1[0] != a2[0]) {
+                return a2[0] - a1[0];
+            }
+            if (a1[1] != a2[1]) {
                 return a2[1] - a1[1];
             }
-            return a2[0] - a1[0];
+            return a2[2] - a1[2];
         });
         for (int i = 0; i < stacks.length; i++) {
             int[] stack = stacks[i];
@@ -36,7 +36,7 @@ class Solution {
                 result += stack[0] + stack[1] + stack[2];
             } else if (picks[1]-- > 0) {
                 result += stack[0] * 5 + stack[1] + stack[2];
-            } else if (picks[2]-- > 0) {
+            } else {
                 result += stack[0] * 25 + stack[1] * 5 + stack[2];
             }
         }
