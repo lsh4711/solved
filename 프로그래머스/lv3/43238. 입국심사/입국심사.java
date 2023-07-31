049233e09db552cd54160f10d25f74ebf25b9932
lsh4711 <120231876+lsh4711@users.noreply.github.com>
2023-08-01 05:15:32 +0900

    [level 3] Title: 입국심사, Time: 0.40 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43238. 입국심사/입국심사.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+class Solution {
+    public long solution(int n, int[] times) {
+        Arrays.sort(times);
+        
+        long start = 1;
+        long end = (long)1e18;
+        
+        while(start < end) {
+            long mid = (start + end) / 2;
+            boolean isEnough = check(n, mid, times);
+            if (!isEnough) {
+                start = mid + 1;
+            } else {
+                end = mid;
+            }
+        }
+        
+        return start;
+    }
+    
+    static boolean check(int n, long mid, int[] times) {
+        long cnt = 0;
+        
+        for (int time : times) {
+            cnt += mid / time;
+            if (cnt >= n) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+}
