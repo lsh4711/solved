2a31762ecdebf88366c82a1f5ba36a7e31c57c62
lsh4711 <120231876+lsh4711@users.noreply.github.com>
2023-08-02 13:32:02 +0900

    [level 3] Title: 가장 긴 팰린드롬, Time: 26.45 ms, Memory: 87 MB -BaekjoonHub

--- 프로그래머스/lv3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.java
+++ 프로그래머스/lv3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.java
@@ -1,31 +1,38 @@
 class Solution {
+    static int max;
+
     public int solution(String s) {
-        int max = 1;
-        boolean[][] dp = new boolean[s.length()][s.length()];
+        int[][] dp = new int[s.length()][s.length()];
         
-        for (int i = 0; i < s.length(); i++) {
-            dp[i][i] = true;
-        }
-        for (int i = 0; i < s.length() - 1; i++) {
-            char left = s.charAt(i);
-            char right = s.charAt(i + 1);
-            if (left == right) {
-                dp[i][i + 1] = true;
-                max = 2;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (max >= i + 1) {
+                break;
             }
-        }
-        for (int i = 2; i < s.length(); i++) {
             for (int j = 0; j < s.length() - i; j++) {
-                char left = s.charAt(j);
-                char right = s.charAt(j + i);
-                if (left != right || !dp[j + 1][j + i - 1]) {
-                    continue;
-                }
-                dp[j][j + i] = true;
-                max = i + 1;
+                int left = j;
+                int right = j + i;
+                recursive(dp, s, left, right);
             }
         }
         
         return max;
+    }
+    
+    static int recursive(int[][] dp, String s, int left, int right) {
+        if (dp[left][right] != 0) {
+            return dp[left][right];
+        }
+        if (left == right || left - right == 1) {
+            max = left - right + 1;
+            return dp[left][right] = 1;
+        }
+        if (s.charAt(left) == s.charAt(right)) {
+            int result  = recursive(dp, s, left + 1, right - 1);
+            if (result == 1) {
+                max = right - left + 1;
+                return dp[left][right] = 1;
+            }
+        }
+        return dp[left][right] = -1;
     }
 }
