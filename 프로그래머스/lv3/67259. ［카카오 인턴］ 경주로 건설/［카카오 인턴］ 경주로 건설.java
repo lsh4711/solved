e200c196af7ffae1b5cb0890c0e8807c542f240b
lsh4711 <120231876+lsh4711@users.noreply.github.com>
2023-08-10 22:18:15 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 0.65 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/lv3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
+++ 프로그래머스/lv3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
@@ -1,32 +1,34 @@
+import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.PriorityQueue;
 
 class Solution {
-    static int max = (int)1e9;
-
     public int solution(int[][] board) {
         int N = board.length;
+        int max = (int)1e9;
         int[][] moves = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-        
         int[][][] routes = new int[N][N][4];
-        routes[0][0] = new int[] {1, 1, 1, 1};
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                int[] costs = routes[i][j];
+                Arrays.fill(costs, max);
+            }
+        }
+        Arrays.fill(routes[0][0], 0);
 
         PriorityQueue<int[]> pq = new PriorityQueue<>((a1, a2) -> a1[2] - a2[2]);
         pq.add(new int[] {0, 0, 0});
-
-        ArrayList<int[]> costList = new ArrayList<>();
 
         while (!pq.isEmpty()) {
             int[] now = pq.poll();
             int row = now[0];
             int col = now[1];
-            costList.clear();
-            addCostsAndSort(costList, routes[row][col]);
             for (int i = 0; i < 4; i++) {
                 int[] move = moves[i];
                 int nextRow = row + move[0];
                 int nextCol = col + move[1];
-                int min = getMinCost(costList, i);
+                int min = getMinCost(routes[row][col], i, max);
                 if (nextRow < 0 || nextCol < 0 || nextRow >= N || nextCol >= N || board[nextRow][nextCol] == 1
                         || routes[nextRow][nextCol][i] != 0 && routes[nextRow][nextCol][i] <= min) {
                     continue;
@@ -39,32 +41,17 @@ class Solution {
         int min = max;
         
         for (int cost : routes[N - 1][N - 1]) {
-            if (0 < cost && cost < min) {
+            if (cost < min) {
                 min = cost;
             }
         }
         
-        return min - 1;
+        return min;
     }
     
-    static void addCostsAndSort(ArrayList<int[]> costList, int[] costs) {
+    static int getMinCost(int[] costs, int direction, int min) {
         for (int i = 0; i < 4; i++) {
-            int cost = costs[i];
-            if (cost > 0) {
-                costList.add(new int[] {i, cost});
-            }
-        }
-        costList.sort((a1, a2) -> a1[1] - a2[1]);
-    }
-    
-    static int getMinCost(ArrayList<int[]> costList, int direction) {
-        int min = max;
-
-        for (int[] costInfo : costList) {
-            int cost = costInfo[1] + 100;
-            if (costInfo[0] != direction) {
-                cost += 500;
-            }
+            int cost = i == direction ? costs[i] + 100 : costs[i] + 600;
             if (cost < min) {
                 min = cost;
             }
