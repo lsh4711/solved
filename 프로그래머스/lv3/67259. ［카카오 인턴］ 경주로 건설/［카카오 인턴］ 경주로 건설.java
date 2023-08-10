45b78fbb4962199dbc0be7bbc8099639bc0a46ed
lsh4711 <120231876+lsh4711@users.noreply.github.com>
2023-08-10 21:48:03 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 1.31 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/lv3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
+++ 프로그래머스/lv3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
@@ -2,65 +2,74 @@ import java.util.ArrayList;
 import java.util.PriorityQueue;
 
 class Solution {
+    static int max = (int)1e9;
+
     public int solution(int[][] board) {
         int N = board.length;
-        int maxCost = (int)1e9;
         int[][] moves = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-
+        
         int[][][] routes = new int[N][N][4];
         routes[0][0] = new int[] {1, 1, 1, 1};
 
         PriorityQueue<int[]> pq = new PriorityQueue<>((a1, a2) -> a1[2] - a2[2]);
         pq.add(new int[] {0, 0, 0});
 
-        ArrayList<int[]> list = new ArrayList<>();
+        ArrayList<int[]> costList = new ArrayList<>();
+
         while (!pq.isEmpty()) {
             int[] now = pq.poll();
             int row = now[0];
             int col = now[1];
-            int totalCost = now[2];
-
-            for (int i = 0; i < 4; i++) {
-                int cost = routes[row][col][i];
-                if (cost == 0) {
-                    continue;
-                }
-                list.add(new int[] {i, cost});
-            }
-            list.sort((a1, a2) -> a1[1] - a2[1]);
-
+            costList.clear();
+            addCostsAndSort(costList, routes[row][col]);
             for (int i = 0; i < 4; i++) {
                 int[] move = moves[i];
                 int nextRow = row + move[0];
                 int nextCol = col + move[1];
-                int minCost = maxCost;
-                for (int[] costInfo : list) {
-                    int cost = costInfo[1] + 100;
-                    if (costInfo[0] != i) {
-                        cost += 500;
-                    }
-                    if (cost < minCost) {
-                        minCost = cost;
-                    }
-                }
+                int min = getMinCost(costList, i);
                 if (nextRow < 0 || nextCol < 0 || nextRow >= N || nextCol >= N || board[nextRow][nextCol] == 1
-                        || routes[nextRow][nextCol][i] != 0 && routes[nextRow][nextCol][i] <= minCost) {
+                        || routes[nextRow][nextCol][i] != 0 && routes[nextRow][nextCol][i] <= min) {
                     continue;
                 }
-                routes[nextRow][nextCol][i] = minCost;
-                pq.add(new int[] {nextRow, nextCol, minCost});
+                routes[nextRow][nextCol][i] = min;
+                pq.add(new int[] {nextRow, nextCol, min});
             }
-            list.clear();
         }
 
-        int minCost = maxCost;
+        int min = max;
         
-        for (int r : routes[N - 1][N - 1]) {
-            if (r != 0 && r < minCost) {
-                minCost = r;
+        for (int cost : routes[N - 1][N - 1]) {
+            if (0 < cost && cost < min) {
+                min = cost;
             }
         }
         
-        return minCost - 1;
+        return min - 1;
+    }
+    
+    static void addCostsAndSort(ArrayList<int[]> costList, int[] costs) {
+        for (int i = 0; i < 4; i++) {
+            int cost = costs[i];
+            if (cost > 0) {
+                costList.add(new int[] {i, cost});
+            }
+        }
+        costList.sort((a1, a2) -> a1[1] - a2[1]);
+    }
+    
+    static int getMinCost(ArrayList<int[]> costList, int direction) {
+        int min = max;
+
+        for (int[] costInfo : costList) {
+            int cost = costInfo[1] + 100;
+            if (costInfo[0] != direction) {
+                cost += 500;
+            }
+            if (cost < min) {
+                min = cost;
+            }
+        }
+
+        return min;
     }
 }
