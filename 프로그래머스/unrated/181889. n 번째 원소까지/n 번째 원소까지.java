87db4252ca8182e278e853f1406069098c1bf711
lsh4711 <120231876+lsh4711@users.noreply.github.com>
2023-08-25 03:41:03 +0900

    [unrated] Title: n 번째 원소까지, Time: 0.02 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181889. n 번째 원소까지/n 번째 원소까지.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        int[] result = Arrays.copyOfRange(num_list, 0, n);
+        
+        return result;
+    }
+}
