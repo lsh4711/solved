914357cd8f86bcc59bcd727271b8554418b648e8
lsh4711 <120231876+lsh4711@users.noreply.github.com>
2023-10-19 12:14:30 +0900

    [level 3] Title: 단속카메라, Time: 7.84 ms, Memory: 58.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[][] routes) {
+        Arrays.sort(routes, (arr1, arr2) -> arr1[0] - arr2[0]);
+        int cnt = 1;
+        int outPoint = routes[0][1];
+        
+        for (int i = 1; i < routes.length; i++) {
+            int[] nextRoute = routes[i];
+            if (outPoint < nextRoute[0]) {
+                outPoint = nextRoute[1];
+                cnt++;
+                continue;
+            }
+            if (outPoint > nextRoute[1]) {
+                outPoint = nextRoute[1];
+            }
+        }
+     
+        return cnt;
+    }
+}
