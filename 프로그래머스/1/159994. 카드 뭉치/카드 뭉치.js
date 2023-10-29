cd35439483f441664056b569f14262cefd5c17a5
lsh4711 <120231876+lsh4711@users.noreply.github.com>
2023-10-29 22:39:07 +0900

    [level 1] Title: 카드 뭉치, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -0,0 +1,22 @@
+function solution(cards1, cards2, goal) {
+    let idx1 = 0;
+    let idx2 = 0;
+    let goalIdx = 0;
+    
+    for (word of goal) {
+        const word1 = cards1[idx1];
+        const word2 = cards2[idx2];
+        if (idx1 < cards1.length && word1 == word) {
+            idx1++;
+            continue;
+        }
+        if (idx2 < cards2.length && word2 == word) {
+            idx2++;
+            continue;
+        } else {
+            return "No";
+        }
+    }
+    
+    return "Yes";
+}
