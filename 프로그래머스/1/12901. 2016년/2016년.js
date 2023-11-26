8e4c46dfbf4f5c892a1a4edfa3db0abe0b44a5b0
lsh4711 <120231876+lsh4711@users.noreply.github.com>
2023-11-26 23:02:49 +0900

    [level 1] Title: 2016년, Time: 0.15 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.js
+++ 프로그래머스/1/12901. 2016년/2016년.js
@@ -2,5 +2,5 @@ function solution(a, b) {
     const days = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
     const idx = new Date(`2016-${a}-${b}`).getDay();
     
-    return days[idx]; 
+    return days[idx];
 }
