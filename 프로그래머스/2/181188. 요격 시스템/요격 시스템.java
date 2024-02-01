bbfe470e0450390faceeb89a4821e0fb018b6253
lsh4711 <120231876+lsh4711@users.noreply.github.com>
2024-02-01 23:53:23 +0900

    [level 2] Title: 요격 시스템, Time: 0.58 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
@@ -0,0 +1,21 @@
+import java.util.PriorityQueue;
+
+class Solution {
+    public int solution(int[][] targets) {
+        int cnt = 0;
+        PriorityQueue<int[]> pq = new PriorityQueue<>((arr1, arr2) -> arr1[1] - arr2[1]);
+        
+        for(int[] target : targets) pq.add(target);
+        while(!pq.isEmpty()) {
+            int[] target = pq.poll();
+            while(!pq.isEmpty()) {
+                int[] nextTarget = pq.peek();
+                if(target[1] <= nextTarget[0]) break;
+                pq.poll();
+            }
+            cnt++;
+        }
+        
+        return cnt;
+    }
+}  
