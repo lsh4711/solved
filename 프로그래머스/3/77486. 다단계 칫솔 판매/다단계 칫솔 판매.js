220aede1bf67d1cb6bfa8a82ab9682d6d2483571
lsh4711 <120231876+lsh4711@users.noreply.github.com>
2024-08-03 23:11:11 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 57.65 ms, Memory: 67.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.js
@@ -0,0 +1,54 @@
+function solution(enroll, referral, seller, amount) {
+    const map = new Map();
+    
+    enroll.forEach(name => {
+        map.set(name, {
+            name,
+            money: 0,
+            profits: [],
+            parent: null,
+            children: []
+        });
+    });
+    
+    referral.forEach((parentName, idx) => {
+        if (parentName === "-") {
+            return;
+        }
+        const childName = enroll[idx];
+        const child = map.get(childName);
+        const parent = map.get(parentName);
+        parent.children.push(child);
+        child.parent = parent;
+    });
+    
+    seller.forEach((seller, idx) => {
+        const profit = amount[idx] * 100;
+        map.get(seller).profits.push(profit);
+    });
+    
+
+    for (let idx = enroll.length - 1; idx >= 0; idx--) {
+        const personName = enroll[idx];
+        const person = map.get(personName);
+        person.profits.forEach((profit, idx) => {
+            if (!profit) {
+                return;
+            }
+            const parentProfit = getParentProfit(profit);
+            person.money += profit - parentProfit;
+            person.parent?.profits.push(parentProfit);
+        });
+    }
+    
+    // enroll.forEach(personName => {
+    //     const {name, profits} = map.get(personName);
+    //     console.log(name, profits);
+    // })
+    
+    return enroll.map(name => map.get(name).money);
+}
+
+const getParentProfit = (profit) => {
+    return parseInt(profit / 10);
+}
