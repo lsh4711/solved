659a147bd0696dc6d81138d2b07bb88a05a51855
lsh4711 <120231876+lsh4711@users.noreply.github.com>
2024-08-14 20:21:30 +0900

    [Gold IV] Title: 공유기 설치, Time: 256 ms, Memory: 28816 KB -BaekjoonHub

---
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -0,0 +1,68 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        new Main().solution();
+    }
+
+    private void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] input = br.readLine().split(" ");
+        int N = Integer.parseInt(input[0]);
+        int C = Integer.parseInt(input[1]);
+
+        int[] houses = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            houses[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(houses);
+
+        if (C == 2) {
+            System.out.println(houses[houses.length - 1] - houses[0]);
+            return;
+        }
+
+        int result = binarySearch(houses, C);
+
+        System.out.println(result);
+    }
+
+    private int binarySearch(int[] houses, int routers) {
+        int start = 0;
+        int end = houses[houses.length - 1] - houses[0];
+
+        while (start < end) {
+            int mid = (start + end) / 2;
+            if (canInstall(houses, routers - 2, mid)) {
+                start = mid + 1;
+            } else {
+                end = mid;
+            }
+        }
+
+        return start - 1;
+    }
+
+    private boolean canInstall(int[] houses, int routers, int minDistance) {
+        int previousHouse = houses[0];
+
+        for (int house : houses) {
+            if (house - previousHouse < minDistance) {
+                continue;
+            }
+            previousHouse = house;
+            if (--routers == 0) {
+                return (houses[houses.length - 1] - house) >= minDistance;
+            }
+        }
+
+        return false;
+    }
+
+}
