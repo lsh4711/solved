ad9e643c7a3160d1b5086d5543dd2012adc8f6b5
lsh4711 <120231876+lsh4711@users.noreply.github.com>
2024-12-06 01:27:56 +0900

    [level 1] Title: 폰켓몬, Time: 0.83 ms, Memory: 34.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,14 +1,6 @@
 function solution(nums) {
-    const max = parseInt(nums.length / 2);
-    const array = [];
-    let result = 0;
+    const length = Math.trunc(nums.length / 2);
+    const set = new Set(nums);
     
-    for (const num of nums) {
-        if (!array[num]) {
-            result++;
-            array[num] = true;
-        }
-    }
-    
-    return Math.min(result, max);
+    return Math.min(length, set.size);
 }
