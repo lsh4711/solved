ab2215c6458b1cdb1e875b008cf312f1391f10b6
lsh4711 <120231876+lsh4711@users.noreply.github.com>
2024-12-31 11:03:34 +0900

    [Gold V] Title: 평범한 배낭, Time: 208 ms, Memory: 10716 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.js
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.js
@@ -2,12 +2,12 @@ const fs = require('fs');
 const input = fs.readFileSync('/dev/stdin', 'utf8')
     .split('\n')
     .map(line => line.split(' ').map(string => parseInt(string)));
-const [[size, maxWeight], ...objects] = input;
+const [[_size, maxWeight], ...objects] = input;
 const dp = new Array(maxWeight + 1).fill(0);
 
 for (const [weight, worth] of objects) {
-    for (let j = maxWeight; j >= weight; j--) {
-        dp[j] = Math.max(dp[j], dp[j - weight] + worth);
+    for (let i = maxWeight; i >= weight; i--) {
+        dp[i] = Math.max(dp[i - weight] + worth, dp[i]);
     }
 }
 
