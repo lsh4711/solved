0f872d38eb02801a2975648b30aa6c4510c6c7ac
lsh4711 <120231876+lsh4711@users.noreply.github.com>
2025-10-11 11:31:14 +0900

    [level 2] Title: 의상, Time: 0.16 ms, Memory: 61.3 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.kt
+++ 프로그래머스/2/42578. 의상/의상.kt
@@ -1,19 +1,13 @@
-import java.util.HashMap
-
 class Solution {
     fun solution(clothes: Array<Array<String>>): Int {
         val map = HashMap<String, Int>()
-        
+
         clothes.forEach {
-            val category = it[1]
-            map[category] = (map[category] ?: 1) + 1
+            val (_, type) = it
+            val cnt = map.getOrDefault(type, 1) + 1
+            map.put(type, cnt)
         }
-        
-        
-        var answer = 1
-        
-        map.forEach {answer *= it.value}
-        
-        return answer - 1
+
+        return map.values.reduce { acc, i -> acc * i } - 1
     }
 }
